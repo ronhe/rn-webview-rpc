66635d30b987c89fb7b9c138bbd299c9c29dd4d8
Ron Heimann <ron.heimann@gmail.com>
2018-04-01 13:58:44 +0300

    Working on injection of script to WebView

--- example/App.js
+++ example/App.js
@@ -2,17 +2,19 @@ import React from 'react';
 import { View, Alert, NetInfo } from 'react-native';
 import WebViewRpc from 'rn-webview-rpc';
 import html from './index.html';
+import webEndpoint from './web-endpoint.js.html';
 
 
 export default class App extends React.Component {
   render() {
+    console.log(html.toString())
     return (
       <View style={{ flex: 1 }}>
         <WebViewRpc
           style={{ marginTop: 0, flex: 1 }}
           source={html}
           exposedObj={{ Alert, NetInfo }}
-          injectedJavaScript="console.log('yo');"
+          injectedJavaScript={webEndpoint.toString()}
         />
       </View>
     );
--- src/web-endpoint.js
+++ src/web-endpoint.js
@@ -1,11 +1,43 @@
 /* eslint-env node, browser */
+/* global Comlink, MessageChannelAdapter */
 
+// As found in https://stackoverflow.com/questions/950087/
+function loadScript(url, callback) {
+  // Adding the script tag to the head as suggested before
+  // var head = document.getElementsByTagName('head')[0];
+  const script = document.createElement('script');
+  // script.type = 'text/javascript';
+  script.src = url;
 
-const webEndpoint = {
-  send: data => window.postMessage(data, '*'),
-  addEventListener: (type, listener, options) => (
-    document.addEventListener(type, listener, options)
-  ),
-};
+  // Then bind the event to the callback function.
+  // There are several events for cross browser compatibility.
+  script.onreadystatechange = callback;
+  script.onload = callback;
 
-export default webEndpoint;
+  // Fire the loading
+  document.head.appendChild(script);
+}
+
+function loadScripts(urls, callback) {
+  if (urls.length === 1) {
+    loadScript(urls[0], callback);
+  } else {
+    loadScripts(urls.slice(1), callback);
+  }
+}
+
+function setRnProxy() {
+  window.rnRpc.proxy = Comlink.proxy(MessageChannelAdapter.wrap({
+    send: (data) => { console.log(data); return window.postMessage(data, '*'); },
+    addEventListener: (type, listener, options) =>
+      document.addEventListener(type, listener, options),
+  }));
+}
+
+window.rnRpc = {};
+loadScripts(
+  ['https://cdn.jsdelivr.net/npm/comlinkjs/comlink.global.js',
+    'https://cdn.jsdelivr.net/npm/comlinkjs/messagechanneladapter.global.js',
+  ],
+  setRnProxy,
+);
