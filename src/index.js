d7ba2456cc79446aa4dd316eaccdd220762ef006
Ron Heimann <ron.heimann@gmail.com>
2018-04-07 09:55:46 +0300

    Refactor web-script and rn-rpc

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import WebViewRpc from './webview-rpc';
+import rnRpc from './rn-rpc';
+
+
+export default WebViewRpc;
+export { rnRpc };
--- src/rn-rpc.js
+++ src/rn-rpc.js
@@ -4,13 +4,15 @@ import { Comlink } from 'comlinkjs/comlink.es6';
 import { MessageChannelAdapter } from 'comlinkjs/messagechanneladapter.es6';
 
 
-const rnRpc = {
-  proxy: Comlink.proxy(MessageChannelAdapter.wrap({
+const rnRpc = {};
+
+if (typeof window !== 'undefined') {
+  rnRpc.proxy = Comlink.proxy(MessageChannelAdapter.wrap({
     send: data => window.postMessage(data, '*'),
     addEventListener: document.addEventListener,
-  })),
-  proxyValue: Comlink.proxyValue,
-};
+  }));
+  rnRpc.proxyValue = Comlink.proxyValue;
+}
 
 
 export default rnRpc;
