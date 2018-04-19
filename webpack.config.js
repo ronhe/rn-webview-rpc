b9c99b877efe148b23268854e375c70d4ec3930c
Ron Heimann <ron.heimann@gmail.com>
2018-04-19 18:29:36 +0300

    remove babel-polfill entry point from webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: ['babel-polyfill', './src/web-script.js'],
+  entry: ['./src/web-script.js'],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
