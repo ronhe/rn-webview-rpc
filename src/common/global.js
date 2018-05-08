d51599c43d4ca1b11280e8d4fb0a95f89bf08468
Ron Heimann <ron.heimann@gmail.com>
2018-05-08 21:35:37 +0300

    Add ArrayBuffer polyfill to support Android 4.4/API 19

--- src/common/global.js
+++ src/common/global.js
@@ -5,6 +5,7 @@ import ObjectPolyfill from 'core-js/es6/object'; // eslint-disable-line import/n
 import SymbolPolyfill from 'core-js/es6/symbol'; // eslint-disable-line import/no-extraneous-dependencies
 import SetPolyfill from 'core-js/es6/set'; // eslint-disable-line import/no-extraneous-dependencies
 import WeakSetPolyfill from 'core-js/es6/weak-set'; // eslint-disable-line import/no-extraneous-dependencies
+import ArrayBuffer from 'core-js/fn/typed/array-buffer'; // eslint-disable-line import/no-extraneous-dependencies
 import proxyPolyfill from 'proxy-polyfill/src/proxy';
 import { MessageChannelPolyfill, MessagePortPolyfill } from './message-channel-polyfill';
 
@@ -31,6 +32,10 @@ try {
 } catch (error) {
   // Replace with ES6 Object that accepts numbers.
   globalScope.Object = ObjectPolyfill;
+}
+
+if (!('isView' in ArrayBuffer)) {
+  globalScope.ArrayBuffer = ArrayBuffer;
 }
 
 Object.keys(polyfills).forEach((p) => {
