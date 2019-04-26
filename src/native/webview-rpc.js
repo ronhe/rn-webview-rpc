3d27885b3d686dec59298323c9cc20a9f744b692
Ron Heimann <ron.heimann@gmail.com>
2019-04-26 15:00:26 +0300

    Add a comment about 'react-native-webview'

--- src/native/webview-rpc.js
+++ src/native/webview-rpc.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react';
+// TODO: Use external 'react-native-webview', since WebView will be extracted
+//  from 'react-native' in future releases.
+//  See https://github.com/react-native-community/react-native-webview.
+//  Keep in mind that it would break the Expo example.
+//  Moreover, the postMessage api is also going to be removed :(
 import { WebView } from 'react-native';
 import PropTypes from 'prop-types';
 import '../common/global';
