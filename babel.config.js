ca415a1345fad8d4fb0e8edbf24b3d2c6f3a15be
Ron Heimann <ron.heimann@gmail.com>
2019-06-09 16:14:03 +0300

    Replace .babelrc with babel.config.js

---
+++ babel.config.js
@@ -0,0 +1,34 @@
+module.exports = function (api) {
+    api.cache(true);
+
+    const presets = [
+        ["@babel/preset-env", {
+            "targets": {
+                "browsers": [
+                    "last 2 versions", "ios >= 7", "android >= 4.4"
+                ]
+            }
+        }],
+        "@babel/preset-react"
+    ];
+
+    const plugins = [
+        ["@babel/plugin-transform-runtime", {
+            "helpers": false,
+            "corejs": 3,
+            "regenerator": true
+        }],
+        "@babel/plugin-syntax-dynamic-import",
+        "@babel/plugin-syntax-import-meta",
+        ["@babel/plugin-proposal-class-properties", { "loose": false }],
+        "@babel/plugin-proposal-json-strings"
+    ];
+
+    const sourceType = 'unambiguous';
+
+    return {
+        presets,
+        plugins,
+        sourceType
+    };
+}
